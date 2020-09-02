dd9a602da5cf7f7f241e0ae80cee3fccc50074a8
ppetrashov <ppetrashov@healthgorilla.com>
2020-09-02 14:37:30 +0300

    add RecursivelyBinarySearcher

---
+++ src/main/java/com/pavel/example/RecursivelyBinarySearcher.java
@@ -0,0 +1,27 @@
+package com.pavel.example;
+
+public class RecursivelyBinarySearcher {
+
+    private final int[] arr;
+
+    public RecursivelyBinarySearcher(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int find(int value) {
+        return recFind(0, arr.length, value);
+    }
+
+    private int recFind(int lowerBound, int upperBound, int searchValue) {
+        int current = (lowerBound + upperBound) / 2;
+
+        if (arr[current] == searchValue) return current;
+        if (lowerBound > upperBound) return -1;
+
+        if (searchValue > arr[current]) {
+            return recFind(current + 1, upperBound, searchValue);
+        } else {
+            return recFind(lowerBound, current - 1, searchValue);
+        }
+    }
+}
---
+++ src/test/java/com/pavel/example/RecursivelyArraySorterTest.java
@@ -0,0 +1,24 @@
+package com.pavel.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RecursivelyArraySorterTest {
+
+    @Test
+    public void findIndexByValue() {
+        RecursivelyBinarySearcher binarySearcher = new RecursivelyBinarySearcher(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+
+        assertEquals(0, binarySearcher.find(1));
+        assertEquals(1, binarySearcher.find(2));
+        assertEquals(2, binarySearcher.find(3));
+        assertEquals(3, binarySearcher.find(4));
+        assertEquals(4, binarySearcher.find(5));
+        assertEquals(5, binarySearcher.find(6));
+        assertEquals(6, binarySearcher.find(7));
+        assertEquals(7, binarySearcher.find(8));
+        assertEquals(8, binarySearcher.find(9));
+        assertEquals(9, binarySearcher.find(10));
+    }
+}
